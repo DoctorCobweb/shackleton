f8deec9f2a75cc55291d34a69008359d283c2fd8
Doctor Cobweb <spinninghalf@gmail.com>
2013-09-22 13:11:41 +1000

    ibid

--- server.js
+++ server.js
@@ -189,7 +189,7 @@ app.post('/api/admin_emails', function (req, res) {
   console.log('in POST /api/admin_emails and body posted is:');
   console.log(req.body);
 
-  email_services.welcome_email_with_body('doctor cobwebl', 'spinninghalf', req.body);
+  email_services.send_welcome_email_with_body('doctor cobwebl', 'spinninghalf', req.body);
   
   res.send(req.body);
 
